1a131dfc9d2bcac08273d5c174ac739d9317bb7d
Ayudya <Tabitha.Ayudya@team.telstra.com>
2021-12-06 11:05:58 +1100

    added signup and login

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Routes as Switch } from "react-router-dom";
 
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/signup" element={<Signup />} />
       {/* Finally, catch all unmatched routes */}
       <Route element={<NotFound />} />
     </Switch>
